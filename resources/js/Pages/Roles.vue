08905ae590838495ed1079112ca955bf11e5cc69
Hector <programadorjr@coorsamexico.com>
2023-01-04 12:56:36 -0600

    Cambios

--- resources/js/Pages/Roles.vue
+++ resources/js/Pages/Roles.vue
@@ -56,15 +56,15 @@ const submit = () => {
         </template>
 
         <template #>
-            <div class="max-w-7xl mx-auto sm:px-2 md:px-4">
+            <div class="mx-auto max-w-7xl sm:px-2 md:px-4">
                 <div
-                    class="max-w-full bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-2 md:grid md:grid-cols-2 md:gap-2"
+                    class="max-w-full px-4 py-2 overflow-hidden bg-white shadow-xl sm:rounded-lg md:grid md:grid-cols-2 md:gap-2"
                 >
                     <!-- Tabla Roles -->
                     <div>
                         <div>
                             <h2
-                                class="text-center mb-1 font-bold text-xl text-gray-500"
+                                class="mb-1 text-xl font-bold text-center text-gray-500"
                             >
                                 Roles
                             </h2>
@@ -82,15 +82,16 @@ const submit = () => {
                             </template>
                             <template #rows>
                                 <tr
-                                    class="text-gray-700 hover:bg-gray-200 transition cursor-pointer even:bg-gray-100"
+                                    class="text-gray-700 transition cursor-pointer hover:bg-gray-200 even:bg-gray-100"
                                     v-for="r in roles"
+                                    :key="r"
                                 >
                                     <Tbody class="capitalize">{{ r.id }}</Tbody>
                                     <Tbody class="capitalize">{{
                                         r.name
                                     }}</Tbody>
                                     <Tbody
-                                        class="grid gap-1 text-right justify-end"
+                                        class="grid justify-end gap-1 text-right"
                                     >
                                         <ThirdButton
                                             class="w-fit"
@@ -123,15 +124,15 @@ const submit = () => {
                     <div>
                         <div>
                             <h2
-                                class="text-center mb-1 font-bold text-xl text-gray-500"
+                                class="mb-1 text-xl font-bold text-center text-gray-500"
                             >
                                 Permisos
                             </h2>
-                            <div class="flex justify-end items-center gap-2">
+                            <div class="flex items-center justify-end gap-2">
                                 <InputLabel
                                     for="search"
                                     value="Buscar:"
-                                    class="text-md text-gray-700 font-semibold"
+                                    class="font-semibold text-gray-700 text-md"
                                 />
                                 <TextInput
                                     id="search"
@@ -148,8 +149,9 @@ const submit = () => {
                             </template>
                             <template #rows>
                                 <tr
-                                    class="text-gray-700 hover:bg-gray-200 transition cursor-pointer even:bg-gray-100"
+                                    class="text-gray-700 transition cursor-pointer hover:bg-gray-200 even:bg-gray-100"
                                     v-for="p in permisos"
+                                    :key="p"
                                     for="permiso"
                                 >
                                     <Tbody class="capitalize">{{ p.id }}</Tbody>
@@ -174,22 +176,22 @@ const submit = () => {
     <!-- Modal del create -->
     <DialogModal class="max-h-screen" :show="modal" @close="closeModal">
         <template #title>
-            <h1 class="font-semibold text-2xl text-gray-600 text-center">
+            <h1 class="text-2xl font-semibold text-center text-gray-600">
                 Roles
             </h1>
         </template>
         <template #content>
-            <form class="p-2 grid grid-cols-2 gap-2" @submit.prevent="submit">
+            <form class="grid grid-cols-2 gap-2 p-2" @submit.prevent="submit">
                 <div>
                     <InputLabel
                         for="name"
                         value="Nombre del Rol"
-                        class="text-md font-semibold"
+                        class="font-semibold text-md"
                     />
                     <TextInput
                         id="name"
                         type="text"
-                        class="mt-1 block w-full"
+                        class="block w-full mt-1"
                         required
                         autofocus
                         v-model="form.name"
@@ -204,8 +206,9 @@ const submit = () => {
                         </template>
                         <template #rows>
                             <tr
-                                class="text-gray-700 hover:bg-gray-200 transition cursor-pointer even:bg-gray-100"
+                                class="text-gray-700 transition cursor-pointer hover:bg-gray-200 even:bg-gray-100"
                                 v-for="p in permisos"
+                                :key="p"
                             >
                                 <Tbody class="capitalize">{{ p.name }}</Tbody>
                                 <Tbody class="text-center w-7">
@@ -220,7 +223,7 @@ const submit = () => {
                     <TextInput
                         id="name"
                         type="hidden"
-                        class="mt-1 block w-full"
+                        class="block w-full mt-1"
                         required
                         autofocus
                         v-model="form.id"
@@ -239,13 +242,13 @@ const submit = () => {
     <!-- Modal Delete -->
     <DialogModal :show="del" @close="closeModal">
         <template #title>
-            <h1 class="font-semibold text-2xl text-gray-600 text-center">
+            <h1 class="text-2xl font-semibold text-center text-gray-600">
                 Rol
                 <span class="font-bold uppercase">{{ datos.name }}.</span>
             </h1>
         </template>
         <template #content>
-            <p class="text-center font-semibold text-lg text-gray-500">
+            <p class="text-lg font-semibold text-center text-gray-500">
                 Estas seguro que deseas eliminar el rol
                 <span class="font-bold uppercase">{{ datos.name }}</span>
                 una vez eliminado no podras recuperarlo
